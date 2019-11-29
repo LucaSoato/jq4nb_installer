6766ee6210265cf505c72375e44f2248daae588f
YYE4106 <luca.soato.consulente@infocert.it>
2019-11-29 11:27:12 +0100

    Yo, genesis commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # jq4nb_installer
 Jq for jupyter notebooks install script 
+
+System git filter installer
+```bash
+wget -qO- 
+
+```
---
+++ install_jq.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget -c https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O /usr/bin/jq
+chmod 770 /usr/bin/jq
